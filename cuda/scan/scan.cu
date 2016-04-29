822c880cf7516e47f3563f26ecafd08fb73de83a
emile cadorel <ecadorel@gmail.com>
2016-04-29 11:40:21 +0200

    scan v2

--- cuda/scan/scan.cu
+++ cuda/scan/scan.cu
@@ -1,29 +1,20 @@
-
-#define NUM_BANKS 16  
-#define LOG_NUM_BANKS 4  
-#define CONFLICT_FREE_OFFSET(n) ((n) >> NUM_BANKS + (n) >> (2 * LOG_NUM_BANKS))  
+#include <iostream>
 
 __global__ void scan(float *g_odata, float *g_idata, int n)  {  
     extern __shared__ float temp[]; 
 
     int thid = threadIdx.x;
     int offset = 1;
-
-    int ai = thid;  
-    int bi = thid + (n/2);  
-    int bankOffsetA = CONFLICT_FREE_OFFSET(ai);
-    int bankOffsetB = CONFLICT_FREE_OFFSET(bi);
-    temp[ai + bankOffsetA] = g_idata[ai];
-    temp[bi + bankOffsetB] = g_idata[bi];
+    temp[2*thid] = g_idata[2*thid]; 
+    temp[2*thid+1] = g_idata[2*thid+1];  
 	
-    for (int d = n >> 1; d > 0; d >>= 1) {   
+    for (int d = n >> 1; d > 0; d >>= 1) {
+	
 	__syncthreads();  
 	if (thid < d) {
+
 	    int ai = offset*(2*thid+1)-1;  
-	    int bi = offset*(2*thid+2)-1;  
-	    ai += CONFLICT_FREE_OFFSET(ai);  
-	    bi += CONFLICT_FREE_OFFSET(bi);
-	    
+	    int bi = offset*(2*thid+2)-1;  	    
 	    temp[bi] += temp[ai];  
 	}	
 	offset *= 2;
@@ -34,25 +25,22 @@ __global__ void scan(float *g_odata, float *g_idata, int n)  {
  
     for (int d = 1; d < n; d *= 2) {  
 	offset >>= 1;  
+
 	__syncthreads();  
 	if (thid < d) {
-
 	    int ai = offset*(2*thid+1)-1;  
 	    int bi = offset*(2*thid+2)-1;  
-	    ai += CONFLICT_FREE_OFFSET(ai);  
-	    bi += CONFLICT_FREE_OFFSET(bi);
-	    
 	    float t = temp[ai];  
 	    temp[ai] = temp[bi];  
 	    temp[bi] += t;   
-	}  
+	}
+	
     }
     
     __syncthreads();
-
-    g_odata[ai] = temp[ai + bankOffsetA];  
-    g_odata[bi] = temp[bi + bankOffsetB];  
     
+    g_odata[2*thid] = temp[2*thid]; 
+    g_odata[2*thid+1] = temp[2*thid+1];  
 }
 
 
